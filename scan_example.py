5e78ae68f02a1d51a0cde7e2ca7a7d098f98c0f4
Daniyal Ansari <daniyal.s.ansari@gmail.com>
2019-05-03 03:21:46 -0500

    commented scan_example.py

--- scan_example.py
+++ scan_example.py
@@ -1,5 +1,10 @@
+# Import pytim5xx Library as "lidar"
 import pytim5xx as lidar
 
+# Repeat code nested in for loop 10 times
 for x in range(10):
-	distances = lidar.scan()
-	print(distances)
+
+	distances = lidar.scan()	# Requests and returns list of LIDAR
+								# distance information in meters
+
+	print(distances)	# print distance list
