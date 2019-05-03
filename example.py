8bde73e07dd92399d50a6bd6765610ca15eeff61
Demo User <debian@scuttle.localdomain>
2019-05-03 04:40:34 +0000

    Added Telegram Breakdown

--- example.py
+++ example.py
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import os
+import struct
+import ctypes
 import pytim5xx as lidar   #   Import TIM5xx Library
+
+
+
+
 
 # reset = lidar.rstoutpcnt()      # Reset LIDAR
 # print(reset)
@@ -8,8 +17,74 @@ import pytim5xx as lidar   #   Import TIM5xx Library
 # data = lidar.scan()
 # print(data)
 
+def uint32(i):
+    i = struct.unpack('>i', bytes.fromhex(i))[0]
+    return i
+
+os.system('clear')
+
 # data = lidar.scan(raw=True)
+data = lidar.demo_data()
+
+# data = data.split()
 # print(data)
+
+# cmd_type = data[0]
+# cmd = data[1]
+# v_num = data[2]
+
+print()
+print("Command Type:\t",data[0])
+print("Command     :\t",data[1])
+print("Version Num :\t",int(data[2],16))
+print("Device Num  :\t",int(data[3],16))
+print("Serial Num  :\t",int(data[4],32))
+print("Device Stat :\t",int(data[6],8))
+print("Telegrm Cnt :\t",int(data[7],16))
+print("Scan Count  :\t",int(data[8],16))
+print()
+print("Time since start (\u03BCs):\t\t",int(data[9],32))
+print("Time of transmission (\u03BCs):\t",int(data[10],32))
+print("Dig Inputs Status:\t\t",int(data[12],8))
+print("Dig Outputs Status:\t\t",int(data[14],8))
+print("Layer Angle :\t\t\t",int(data[15],16))
+print("Scan Frequency (Hz):\t\t",int(data[16],16)/100)
+print("Measurement Frequency (Hz):\t",int(data[17],16)/100) #Math may not be right
+print("Amount of Encoder Data :\t",int(data[18],16))
+print("# of valid 16 bit channels :\t",int(data[19],16))
+
+print("\n\tDistance\n")
+#print(data[20])
+
+print("Scale Factor:\t\t\t",int(data[21],16))
+print("Scale Factor Offset:\t\t",int(data[22],16))
+print("Start Angle:\t\t\t",uint32(data[23])/10000,"°")
+print("Size of single angular step:\t",int(data[24],16)/10000,"°")
+data_amnt = int(data[25],16)
+print("Amount of Data:\t\t\t",data_amnt)
+
+# distances = lidar.scan()
+# print(distances)
+
+print("\n\tRSSI\n")
+# print(data[811+26])
+# print(data[26+data_amnt])
+
+print("Scale Factor:\t\t\t",data[27+data_amnt])
+print("Scale Factor Offset:\t\t",data[28+data_amnt])
+print("Start Angle:\t\t\t",uint32(data[29+data_amnt])/10000,"°")
+print("Size of single angular step:\t",int(data[30+data_amnt],16)/10000,"°")
+rssi_amnt = int(data[31+data_amnt],16)
+print("Amount of Data:\t\t\t",data_amnt)
+
+
+
+
+
+
+
+
+
 
 # for x in range(10):
 #     data = lidar.scan() # Request Reading from LIDAR.
@@ -20,9 +95,6 @@ import pytim5xx as lidar   #   Import TIM5xx Library
 
 # ident = lidar.deviceident()
 # print(ident)
-
-# name = lidar.setLocationName("SCUTTLE-Dev")
-# print(name)
 
 # name = lidar.readLocationName()
 # print(name)
@@ -48,6 +120,9 @@ import pytim5xx as lidar   #   Import TIM5xx Library
 
 # stop = lidar.stopmeas()
 # print(stop)
+
+# name = lidar.setLocationName("SCUTTLE-Dev")
+# print(name)
 
 # factory_def = lidar.loadfacdef()
 # print(factory_def)
