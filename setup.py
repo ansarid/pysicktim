b6a9ddb4eb7afd8d8f2745ecfd1f94106912fb4d
ansarid <daniyal.s.ansari@gmail.com>
2019-09-30 12:43:40 -0500

    added prereq's

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     description='TIM561 Python Library',
     license='GNU General Public License v3.0',
     packages=['pysicktim'],
+    install_requires=['pyusb'],
     author='Daniyal Ansari',
     author_email='daniyal.s.ansari+pypi@gmail.com',
     keywords=['tim561','sick','lidar','sicktim','tim5xx','sicktim5xx','sicktim561'],
