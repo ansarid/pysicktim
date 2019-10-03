405c82efd8bf04c56590f515497da9cdea50ca43
ansarid <daniyal.s.ansari@gmail.com>
2019-10-03 18:54:29 -0500

    fixed some english

--- examples/scan_example.py
+++ examples/scan_example.py
@@ -1,11 +1,11 @@
-# Import pysicktim Library as "lidar"
+# Import pysicktim library as "lidar"
 import pysicktim as lidar
 
 # Repeat code nested in for loop 10 times
 for x in range(10):
 # while True:
 
-	lidar.scan()	# Requests and returns list of LIDAR
-					# distance information in meters
+	lidar.scan()	# Requests and returns list of LiDAR
+			# distance data in meters
 
-	print(lidar.scan.distances)		# print distance list
+	print(lidar.scan.distances)	# print distance list
